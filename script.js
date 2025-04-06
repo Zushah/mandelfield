15599f7502a39d4be5ce7525bf926cfef567c6a0
Zushah <71158194+Zushah@users.noreply.github.com>
2025-04-06 10:48:27 -0400

    Changed

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const ctx = document.getElementById("canvas").getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const cb = Chalkboard;
+const ctx = document.getElementById("canvas").getContext("2d");
+canvas.width = window.innerWidth, canvas.height = window.innerHeight;
 
-cb.PARSEPREFIX = "function Mandelfield(x, y) { let a = x, b = y; for(let i = 0; i < 16; i++) { let aa = a*a - b*b, bb = 2*a*b; a = aa + x, b = bb + y; } return a*a + b*b < 4 ? 1 : -1; }";
-const F = cb.vect.field("-y / Math.sqrt(x*x + y*y) * Mandelfield(x, y)", "x / Math.sqrt(x*x + y*y) * Mandelfield(x, y)");
+cb.PARSEPREFIX = " \
+    const Mandelfield = (x, y) => { \
+        let a = x, b = y; \
+        for (let i = 0; i < 16; i++) { \
+            let aa = a*a - b*b, bb = 2*a*b; \
+            a = aa + x, b = bb + y; \
+        } \
+        return a*a + b*b < 4 ? 1 : -1; \
+    }; \
+";
+const F = cb.vect.field(
+    "-y / Math.sqrt(x*x + y*y) * Mandelfield(x, y)",
+    " x / Math.sqrt(x*x + y*y) * Mandelfield(x, y)"
+);
 class Particle {
     constructor(p) {
         this.pos = p;
@@ -33,23 +44,23 @@ class Particle {
     }
 }
 let particles = [];
-for(let i = -canvas.width / 2 - 100; i < canvas.width / 2 + 100; i += 10) {
-    for(let j = -canvas.height / 2 - 100; j < canvas.height / 2 + 100; j += 10) {
+for (let i = -canvas.width / 2 - 100; i < canvas.width / 2 + 100; i += 10) {
+    for (let j = -canvas.height / 2 - 100; j < canvas.height / 2 + 100; j += 10) {
         particles.push(new Particle(cb.vect.init(i, j)));
     }
 }
 
 ctx.fillStyle = "rgb(0, 0, 0)";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-function main() {
+const main = () => {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for(let i = 0; i < particles.length; i++) {
+    for (let i = 0; i < particles.length; i++) {
         particles[i].draw();
-        if(cb.vect.magsq(particles[i].dist) > cb.numb.random(100, 10000)) {
+        if (cb.vect.magsq(particles[i].dist) > cb.numb.random(100, 10000)) {
             particles.splice(i, 1, new Particle(particles[i].opos));
         }
     }
     window.requestAnimationFrame(main);
 }
-window.requestAnimationFrame(main);
+main();
